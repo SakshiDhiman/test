c09a9fee8aa6fc531b34482ee7bc105ef59295db
SakshiDhiman <49397348+SakshiDhiman@users.noreply.github.com>
2023-06-13 14:44:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ sudo su -
 cd /var/www/html/
 git clone git@github.com:SakshiDhiman/deploy_nextjs_app.git
 cd deploy_nextjs_app/
-npm install
-npm run build
-pm2 start npm --name "test" -- start
+sudo npm install
+sudo npm run build
+sudo pm2 start npm --name "test" -- start
